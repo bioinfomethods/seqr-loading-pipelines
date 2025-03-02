2e2f1e9b5e7da38b8bd82d86b365c89bdd823e01
tommy.li <tommy.li@firefire.co>
2025-03-03 08:19:56 +1100

    fix: Make it work for Python 3.9

--- v03_pipeline/__init__.py
+++ v03_pipeline/__init__.py
@@ -3,5 +3,5 @@ import datetime
 MAJOR_VERSION = 3
 
 __version__ = (
-    f'{MAJOR_VERSION}.{datetime.datetime.now(datetime.UTC).strftime("%Y%m%d.%H%M%S")}'
+    f'{MAJOR_VERSION}.{datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d.%H%M%S")}'
 )
