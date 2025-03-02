aa7c5c4314d261c8419fe7f2cad717473ed80f4a
tommy.li <tommy.li@firefire.co>
2025-03-03 08:25:20 +1100

    feat: Retrofit some v03 pipeline logic into MCRI pipeline

--- hail_scripts/computed_fields/vep.py
+++ hail_scripts/computed_fields/vep.py
@@ -1,10 +1,57 @@
 import hail as hl
 
+from v03_pipeline.lib.reference_datasets.gencode.mapping_gene_ids import load_gencode_ensembl_to_refseq_id
+
+from v03_pipeline.lib.annotations.enums import (
+    BIOTYPES,
+    FIVEUTR_CONSEQUENCES,
+    LOF_FILTERS,
+    TRANSCRIPT_CONSEQUENCE_TERMS,
+)
+
+BIOTYPE_LOOKUP = hl.dict(hl.enumerate(BIOTYPES, index_first=False))
+EXTENDED_INTRONIC_SPLICE_REGION_VARIANT = 'extended_intronic_splice_region_variant'
+FIVEUTR_CONSEQUENCES_LOOKUP = hl.dict(
+    hl.enumerate(FIVEUTR_CONSEQUENCES, index_first=False).extend(
+        [(hl.missing(hl.tstr), hl.missing(hl.tint32))],
+    ),
+)
+LOF_FILTERS_LOOKUP = hl.dict(hl.enumerate(LOF_FILTERS, index_first=False))
+MANE_SELECT_ANNOTATIONS = [
+    'mane_select',
+    'mane_plus_clinical',
+]
+NAGNAG_SITE = 'NAGNAG_SITE'
+OMIT_TRANSCRIPT_CONSEQUENCE_TERMS = hl.set(
+    [
+        'upstream_gene_variant',
+        'downstream_gene_variant',
+    ],
+)
+PROTEIN_CODING_ID = BIOTYPE_LOOKUP['protein_coding']
+SELECTED_ANNOTATIONS = [
+    'amino_acids',
+    'canonical',
+    'codons',
+    'gene_id',
+    'hgvsc',
+    'hgvsp',
+    'transcript_id',
+]
+TRANSCRIPT_CONSEQUENCE_TERMS_LOOKUP = hl.dict(
+    hl.enumerate(TRANSCRIPT_CONSEQUENCE_TERMS, index_first=False),
+)
+
+GENCODE_FOR_VEP_RELEASE = 44
+gencode_ensembl_to_refseq_id_mapping = hl.literal(load_gencode_ensembl_to_refseq_id(GENCODE_FOR_VEP_RELEASE))
+
 NON_CODING_TRANSCRIPT_EXON_VARIANT = "non_coding_transcript_exon_variant"
 
 # Consequence terms in order of severity (more severe to less severe) as estimated by Ensembl.
 # See https://ensembl.org/info/genome/variation/prediction/predicted_data.html
 CONSEQUENCE_TERMS = [
+    "am_class",
+    "am_pathogenicity",
     "transcript_ablation",
     "splice_acceptor_variant",
     "splice_donor_variant",
@@ -54,6 +101,17 @@ OMIT_CONSEQUENCE_TERMS = [
     "upstream_gene_variant",
     "downstream_gene_variant",
 ]
+
+def _lof_filter_ids(c: hl.StructExpression) -> hl.ArrayNumericExpression:
+    return hl.or_missing(
+        (c.lof == 'LC') & hl.is_defined(c.lof_filter),
+        c.lof_filter.split('&|,').map(lambda f: LOF_FILTERS_LOOKUP[f]),
+    )
+
+def _consequence_term_ids(c: hl.StructExpression) -> hl.ArrayNumericExpression:
+    return c.consequence_terms.filter(
+        lambda t: ~OMIT_TRANSCRIPT_CONSEQUENCE_TERMS.contains(t),
+    ).map(lambda t: TRANSCRIPT_CONSEQUENCE_TERMS_LOOKUP[t])
 
 def get_expr_for_vep_consequence_terms_set(vep_transcript_consequences_root):
     vep_consequence_terms_set = hl.set(vep_transcript_consequences_root.flatmap(lambda c: c.consequence_terms))
@@ -171,12 +229,10 @@ def get_expr_for_vep_sorted_transcript_consequences_array(vep_root,
         "cdna_start",
         "cdna_end",
         "codons",
-        "exon",
         "gene_id",
         "gene_symbol",
         "hgvsc",
         "hgvsp",
-        "intron",
         "transcript_id",
     ]
 
@@ -198,16 +254,110 @@ def get_expr_for_vep_sorted_transcript_consequences_array(vep_root,
     omit_consequence_terms = hl.set(omit_consequences) if omit_consequences else hl.empty_set(hl.tstr)
 
     result = hl.sorted(
+        # vep_root.transcript_consequences.map(
+        #     lambda c: c.select(
+        #         *selected_annotations,
+        #         alphamissense=hl.struct(
+        #             pathogenicity=c.am_pathogenicity,
+        #         ),
+        #         consequence_terms=c.consequence_terms.filter(lambda t: ~omit_consequence_terms.contains(t)),
+        #         domains=c.domains.map(lambda domain: domain.db + ":" + domain.name),
+        #         major_consequence=hl.cond(
+        #             c.consequence_terms.size() > 0,
+        #             hl.sorted(c.consequence_terms, key=lambda t: CONSEQUENCE_TERM_RANK_LOOKUP.get(t))[0],
+        #             hl.null(hl.tstr),
+        #         )
+        #     )
+        # )
         vep_root.transcript_consequences.map(
             lambda c: c.select(
                 *selected_annotations,
+                *MANE_SELECT_ANNOTATIONS,
+                biotype_id=BIOTYPE_LOOKUP[c.biotype],
                 consequence_terms=c.consequence_terms.filter(lambda t: ~omit_consequence_terms.contains(t)),
+                consequence_term_ids=_consequence_term_ids(c),
                 domains=c.domains.map(lambda domain: domain.db + ":" + domain.name),
                 major_consequence=hl.cond(
                     c.consequence_terms.size() > 0,
                     hl.sorted(c.consequence_terms, key=lambda t: CONSEQUENCE_TERM_RANK_LOOKUP.get(t))[0],
                     hl.null(hl.tstr),
-                )
+                ),
+                exon=hl.bind(
+                    lambda split: hl.or_missing(
+                        hl.is_defined(split),
+                        hl.Struct(index=split[0], total=split[1]),
+                    ),
+                    c.exon.split('/').map(hl.parse_int32),
+                ),
+                intron=hl.bind(
+                    lambda split: hl.or_missing(
+                        hl.is_defined(split),
+                        hl.Struct(index=split[0], total=split[1]),
+                    ),
+                    c.intron.split('/').map(hl.parse_int32),
+                ),
+                refseq_transcript_id=gencode_ensembl_to_refseq_id_mapping.get(c.transcript_id),
+                alphamissense=hl.struct(
+                    pathogenicity=c.am_pathogenicity,
+                ),
+                loftee=hl.struct(
+                    is_lof_nagnag=c.lof_flags == NAGNAG_SITE,
+                    lof_filter_ids=_lof_filter_ids(c),
+                ),
+                spliceregion=hl.struct(
+                    extended_intronic_splice_region_variant=(
+                        hl.is_defined(c.spliceregion)
+                        & c.spliceregion.contains(
+                            EXTENDED_INTRONIC_SPLICE_REGION_VARIANT,
+                        )
+                    ),
+                ),
+                utrannotator=hl.struct(
+                    existing_inframe_oorfs=c.existing_inframe_oorfs,
+                    existing_outofframe_oorfs=c.existing_outofframe_oorfs,
+                    existing_uorfs=c.existing_uorfs,
+                    fiveutr_consequence_id=FIVEUTR_CONSEQUENCES_LOOKUP[c.fiveutr_consequence],
+                    # Annotation documentation here:
+                    # https://github.com/ImperialCardioGenetics/UTRannotator?tab=readme-ov-file#the-detailed-annotation-for-each-consequence
+                    # NB:
+                    fiveutr_annotation=c.fiveutr_annotation['1'].annotate(
+                        AltStopDistanceToCDS=hl.parse_int32(
+                            c.fiveutr_annotation['1'].AltStopDistanceToCDS,
+                        ),
+                        CapDistanceToStart=hl.parse_int32(
+                            c.fiveutr_annotation['1'].CapDistanceToStart,
+                        ),
+                        DistanceToCDS=hl.parse_int32(
+                            c.fiveutr_annotation['1'].DistanceToCDS,
+                        ),
+                        DistanceToStop=hl.parse_int32(
+                            c.fiveutr_annotation['1'].DistanceToStop,
+                        ),
+                        Evidence=hl.or_missing(
+                            # Just in case a weird value ("NA" or anything else) propagates
+                            (
+                                (c.fiveutr_annotation['1'].Evidence == 'True')
+                                | (c.fiveutr_annotation['1'].Evidence == 'False')
+                            ),
+                            hl.bool(c.fiveutr_annotation['1'].Evidence),
+                        ),
+                        StartDistanceToCDS=hl.parse_int32(
+                            c.fiveutr_annotation['1'].StartDistanceToCDS,
+                        ),
+                        newSTOPDistanceToCDS=hl.parse_int32(
+                            c.fiveutr_annotation['1'].newSTOPDistanceToCDS,
+                        ),
+                        alt_type_length=hl.parse_int32(
+                            c.fiveutr_annotation['1'].alt_type_length,
+                        ),
+                        ref_StartDistanceToCDS=hl.parse_int32(
+                            c.fiveutr_annotation['1'].ref_StartDistanceToCDS,
+                        ),
+                        ref_type_length=hl.parse_int32(
+                            c.fiveutr_annotation['1'].ref_type_length,
+                        ),
+                    ),
+                ),
             )
         )
         .filter(lambda c: c.consequence_terms.size() > 0)
